0913318e6085eff9b5d8ed5ce3a920b0f27a2b5c
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-02-13 20:22:18 +0800

    更新GPU

--- predictor.py
+++ predictor.py
@@ -32,8 +32,8 @@ class VideoPredictor(object):
         初始化模型
         """
         model = VSFA()
-        model.load_state_dict(torch.load(self.model_path, map_location=torch.device('cpu')))
-        # model.load_state_dict(torch.load(self.model_path))
+        # model.load_state_dict(torch.load(self.model_path, map_location=torch.device('cpu')))
+        model.load_state_dict(torch.load(self.model_path))
         model.to(self.device)
         model.eval()
 
